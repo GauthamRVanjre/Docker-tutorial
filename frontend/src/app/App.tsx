8cee00369cb3c96395135c814942f6caeda9f5d3
Vikram R Vanjre <vanjrevikram@gmail.com>
2026-04-19 19:28:00 +0530

    initialzed empty react vite app

---
+++ frontend/src/app/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <h1>Welcome to the Docker Tutorial Frontend!</h1>
+      <p>This is a simple React application running inside a Docker container.</p>
+    </div>
+  )
+}
+
+export default App
